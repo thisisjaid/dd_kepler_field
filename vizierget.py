ed3384fe0a2086b252ba350f5c0d84b0c23b63b0
Alex Enachioaie <thisisjaid@gmail.com>
2017-04-06 13:20:17 +0100

    Update vizierget

--- vizierget.py
+++ vizierget.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python3
 
 import sys
+import time
 from astroquery.vizier import Vizier
 import astropy.units as au
 
-ddwise = open("dd_wise.txt","r")
-results = open("results.txt","w")
-poop
-v = Vizier(columns=['AllWISE', 'Jmag'])
+ddcollections = open("dd_target_collections.txt","r")
 
-curline = 0
-nolines = len(ddwise.readlines())
-ddwise.seek(0)
+v = Vizier(columns=['AllWISE', 'RAJ2000', 'DEJ2000'])
 
-for line in ddwise:
-	curline += 1
-	fields = line.strip().split(",")
-	qresult = v.query_object(fields[1],catalog=["AllWISE"],radius=0.001*au.deg) 
-	array_result = qresult[0].as_array()
-	print(fields[0],",",fields[1],",",array_result[0][1],sep='',file=results)
-	progress = (curline / nolines)*100
-	sys.stdout.write("\rNow %d%% done" % int(progress))
-ddwise.close()
-results.close()
+for line in ddcollections:
+
+	line = line.rstrip()
+	ddtowise = open("data/dd_to_wise_ids_"+line+".txt","r")
+	results = open("results/results_"+line+".txt","w")
+	curline = 0
+	nolines = len(ddtowise.readlines())
+	ddtowise.seek(0)
+	sys.stdout.write("Now processing collection %s \n" % line)
+
+	for aline in ddtowise:
+		curline += 1
+		fields = aline.strip().split(",")
+		qresult = v.query_object(fields[1],catalog=["AllWISE"],radius=0.001*au.deg) 
+		array_result = qresult[0].as_array()
+		print(fields[0],",",fields[1],",",array_result[0][1],",",array_result[0][2],sep='',file=results)
+		progress = (curline / nolines)*100
+		sys.stdout.write("\rNow %d%% done" % int(progress))
+		time.sleep(1)
+	ddtowise.close()
+	results.close()
