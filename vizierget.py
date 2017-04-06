2ae747403ab6f40fd98d306a40d72947d2dd75af
Alex Enachioaie <thisisjaid@gmail.com>
2017-04-07 00:14:20 +0100

    Added EPIC check

--- vizierget.py
+++ vizierget.py
@@ -10,6 +10,8 @@ ddcollections = open("dd_target_collections.txt","r")
 
 v = Vizier(columns=['AllWISE', 'RAJ2000', 'DEJ2000', '2Mkey'])
 v2 = Vizier(columns=['KIC', 'fv', '2Mkey'])
+v3 = Vizier(columns=['EPIC'])
+
 
 for line in ddcollections:
 
@@ -18,7 +20,7 @@ for line in ddcollections:
 	if os.path.isfile("data/dd_to_wise_ids_"+line+".txt"): 
 		ddtowise = open("data/dd_to_wise_ids_"+line+".txt","r")
 		results = open("results/results_"+line+".txt","w")
-		print("DD_ID,WISE_ID,RAJ2000,DEJ2000,KIC_ID,KIC_FOV,2MKEY_MISMATCH",file=results)
+		print("DD_ID,WISE_ID,RAJ2000,DEJ2000,KIC_ID,KIC_FOV,2MKEY_MISMATCH,K2_EPIC",file=results)
 		curline = 0
 		nolines = len(ddtowise.readlines())
 		ddtowise.seek(0)
@@ -29,17 +31,32 @@ for line in ddcollections:
 			fields = aline.strip().split(",")
 
 			qresult = v.query_object(fields[1],catalog=["AllWISE"],radius=0.001*au.deg) 
-			array_result = qresult[0].as_array()
+			if qresult:
+				array_result = qresult[0].as_array()
+				resultline = str(fields[0])+","+str(fields[1])+","+str(array_result[0][1])+","+str(array_result[0][2])
+			else:
+				sys.exit(1)
+			
 			time.sleep(1)
+
 			qresult2 = v2.query_object(fields[1],catalog=["V/133"],radius=0.001*au.deg) 
 			if qresult2:
 				array_result2 = qresult2[0].as_array()
 				if str(array_result[0][3]) == str(array_result2[0][2]):
-					print(fields[0],",",fields[1],",",array_result[0][1],",",array_result[0][2],",",array_result2[0][1],",",array_result2[0][0],sep='',file=results)
+					resultline = resultline+","+str(array_result2[0][1])+","+str(array_result2[0][0])+",0"
 				else:
-					print(fields[0],",",fields[1],",",array_result[0][1],",",array_result[0][2],",",array_result2[0][1],",",array_result2[0][0],",","MISS",sep='',file=results)	
+					resultline = resultline+","+str(array_result2[0][1])+","+str(array_result2[0][0])+",1"	
 			else:
-				print(fields[0],",",fields[1],",",array_result[0][1],",",array_result[0][2],",","NA",",","NA",sep='',file=results)
+				resultline = resultline+",NA,NA,NA"
+			
+			qresult3 = v3.query_object(fields[1],catalog=["EPIC"],radius=0.001*au.deg)
+			if qresult3:
+				array_result3 = qresult3[0].as_array()
+				resultline = resultline+","+str(array_result3[0][0])
+			else:
+				resultline = resultline+",NA"
+
+			print(resultline,file=results)
 			progress = (curline / nolines)*100
 			sys.stdout.write("\rNow %d%% done" % int(progress))
 			time.sleep(1)
